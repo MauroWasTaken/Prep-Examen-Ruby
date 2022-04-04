c72cdbcbedf4a5dca9120685bc66ba03485ce025
maurxsantoz <maurx18@gmail.com>
2022-04-04 16:18:48 +0200

    🐬

---
+++ db/migrate/20220404134533_create_order_items.rb
@@ -0,0 +1,10 @@
+class CreateOrderItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_items do |t|
+      t.integer :quantity
+      t.decimal :item_price,precision: 10, scale: 2 
+      t.references :order :foreign_key => true
+      t.references :product
+    end
+  end
+end
